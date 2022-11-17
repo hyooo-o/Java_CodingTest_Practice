e6bd8ac3c474fad4932646953a16fd3df3907a0e
hyooo-o <xozo5939@gmail.com>
2022-11-17 23:58:53 +0900

    SWEA 8382 방향전환 D4 수학적 구현

--- 수학/Solution_8382_김정효.java
+++ 수학/Solution_8382_김정효.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution_8382_김정효 {
-	static int x1, x2, y1, y2;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,27 +13,26 @@ public class Solution_8382_김정효 {
 		
 		for (int tc = 1; tc <= t; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			x1 = Integer.parseInt(st.nextToken());
-			y1 = Integer.parseInt(st.nextToken());
-			x2 = Integer.parseInt(st.nextToken());
-			y2 = Integer.parseInt(st.nextToken());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
 			
 			int x = Math.abs(x1-x2);
 			int y = Math.abs(y1-y2);
 			int result = 0;
 			
-			if (Math.max(x, y)%2 == 0) {
-				result = Math.max(x, y)*2;
+			if (x == y) {
+				result = x+y;
 			} else {
-				if (x != y) {
-					result = Math.max(x, y)*2-1;
+				if (x%2 == y%2) {
+					result = Math.max(x, y)*2;
 				}else {
-					result = x+y;
+					result = Math.max(x, y)*2-1;
 				}
 			}
 			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
-
 }
