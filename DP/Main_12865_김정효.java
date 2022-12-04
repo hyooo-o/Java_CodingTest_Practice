d3f558666948dac550e2b251e36b57073f844a11
hyooo-o <xozo5939@gmail.com>
2022-12-05 00:31:02 +0900

    BOJ 12865 배낭 골드5

--- DP/Main_12865_김정효.java
+++ DP/Main_12865_김정효.java
@@ -12,7 +12,7 @@ public class Main_12865_김정효 {
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		bag = new int[n][2];
-		dp = new int[n];
+		dp = new int[k+1];
 		max = 0;
 		
 		for (int i = 0; i < n; i++) {
@@ -21,37 +21,13 @@ public class Main_12865_김정효 {
 			bag[i][1] = Integer.parseInt(st.nextToken());	// 가치
 		}
 		
-		select(k, 1);
-		System.out.println(dp[n-1]);
-	}
-	/**
-	 * 부분집합으로 배낭에 넣을 물건 선택
-	 * @param w 무게
-	 * @param v 가치
-	 * @param index 인덱스
-	 */
-	private static void select(int w, int index) {
-		// 모든 물건에 대해 선택/미선택 완료하면 최대 가치 구하기
-		if (index == n) {
-			return;
-		}
-		
-		// 무게 초과면 리턴
-		if (w-bag[index][0] >= 0) {
-			if (dp[index] < dp[index-1]+bag[index][1]) {
-				// 선택
-				dp[index] =  dp[index-1] + bag[index][1];
-				select(w-bag[index][0], index+1);
+		for (int i = 0; i < n; i++) {	// 물건
+			for (int j = k; j-bag[i][0] >= 0; j--) {	// 무게k부터 k-물건무게까지
+				// 현재 무게의 가치 = max(현재무게의 가치, (전체무게-물건i의 무게)=남은 무게의 가치 + 물건i의 가치)
+				dp[j] = Math.max(dp[j], dp[j-bag[i][0]] + bag[i][1]);
 			}
-			// 미선택
-			select(w, index+1);
 		}
-		else {
-			if (dp[index] < dp[index-1]) {
-				dp[index] = dp[index-1];
-			}
-			select(w, index+1);
-		}
+		System.out.println(dp[k]);
 	}
-
+	
 }
