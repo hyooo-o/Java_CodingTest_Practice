c6e5e98e6a4e90ae1d0c9b05fedb20e6d9b0263c
hyooo-o <xozo5939@gmail.com>
2022-12-08 23:42:56 +0900

    boj 16926 배열돌리기1 실버1

--- 시뮬레이션/Main_16926_김정효.java
+++ 시뮬레이션/Main_16926_김정효.java
@@ -8,8 +8,7 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_16926_김정효 {
-	static int m, n, map[][], r;
-	static List<Integer> list = new ArrayList<>();
+	static int m, n, map[][], r, cnt;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,22 +16,56 @@ public class Main_16926_김정효 {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		r = Integer.parseInt(st.nextToken());
+		cnt = Math.min(n, m) / 2;	// 그룹 수
 		
 		map = new int[n][m];
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < m; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (i<n/2 && j<m/2 && i==j) list.add(map[i][j]);
 			}
 		}
-		cycle();
+		cycle();	// 회전
+		print();	// 출력
 	}
 
 	private static void cycle() {
-		// 상우하좌
-		int[] dx = {-1, 0, 1, 0};
-		int[] dy = {0, 1, 0, -1};
+		// 우하좌상
+		int[] dx = {0, 1, 0, -1};
+		int[] dy = {1, 0, -1, 0};
+		
+		// 전체 회전 수
+		for (int k = 0; k < r; k++) {
+			// 그룹 개수만큼 반시계방향으로 돌리기
+			for (int i = 0; i < cnt; i++) {
+				int x = i;
+				int y = i;
+				int temp = map[i][i];	// 시작점
+				// 4방향 돌리기
+				int d = 0;
+				while (d < 4) {
+					int nx = x + dx[d];
+					int ny = y + dy[d];
+					// 주의! : 그룹에 따라 범위가 유동적임!
+					if (nx>=i && ny>=i && nx<n-i && ny<m-i) {
+						map[x][y] = map[nx][ny];	// 반시계방향
+						x = nx;
+						y = ny;
+					}
+					else d++;
+				}
+				// 한 그룹 다 돌면 시작점도 옮겨주기
+				map[i+1][i] = temp;
+			}
+		}
 	}
 
+	private static void print() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
