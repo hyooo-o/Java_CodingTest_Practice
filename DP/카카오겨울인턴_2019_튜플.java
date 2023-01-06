0546483d0dcf16ca2bc43d089ba6b38da91f4ff4
hyooo-o <72725382+hyooo-o@users.noreply.github.com>
2023-01-06 23:35:46 +0900

    -ing

---
+++ DP/카카오겨울인턴_2019_튜플.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class 카카오겨울인턴_2019_튜플 {
+	public static void main(String[] args) {
+		solution("{{2}, {3, 5}}");
+	}
+	
+	public static int[] solution(String s) {
+        int[] answer = {};
+        List<List<Integer>> list = new ArrayList<>();
+        char[] c = new char[s.length()-2];
+        for (int i = 1; i < s.length()-1; i++) {
+			c[i-1] = s.charAt(i);
+		}
+        
+        int cnt = 0;
+        while(cnt < c.length) {
+        	if (c[cnt] == '{') {
+        		cnt++;
+        		List<Integer> arr = new ArrayList<>();
+        		while(c[cnt] != '}') {
+        			if (c[cnt]-'0' > 0) arr.add(c[cnt]-'0');
+        			cnt++;
+        		}
+        		list.add(arr);
+        	}
+        	cnt++;
+        }
+        return answer;
+    }
+}
